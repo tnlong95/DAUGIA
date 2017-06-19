c29e952e408439363bd10a94c26a8f1270465183
vinhle95 <ltvinh1295@gmail.com>
2017-06-19 14:41:45 +0700

    Cap nhat view quan ly nguoi dung va san pham

---
+++ DauGia/Controllers/NguoiDungController.cs
@@ -0,0 +1,27 @@
+﻿using DauGia.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DauGia.Controllers
+{
+    public class NguoiDungController : Controllers
+    {
+        // GET: NguoiDung
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult PartialDanhSachNguoiDung()
+        {
+            using (DauGiaEntities ctx = new DauGiaEntities())
+            {
+                var lstNguoiDung = ctx.NguoiDung.ToList();
+                return PartialView("DanhSachNguoiDung", lstNguoiDung);
+            }
+        }
+    }
+}
